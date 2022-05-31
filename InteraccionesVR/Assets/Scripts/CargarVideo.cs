913dbd66fce880c6fbec03502397c5d9114f8676
Joseph Valenciano <jvalencianom08@yahoo.com>
2022-05-30 20:27:21 -0600

    Agregando Intro y Menú

---
+++ InteraccionesVR/Assets/Scripts/CargarVideo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+
+public class CargarVideo : MonoBehaviour
+{
+    private IEnumerator couroutine;
+    public VideoPlayer mivideo;
+    public void Cargando()
+    {
+    couroutine = WaitVideoandLoadScene(59.0f) ;
+    StartCoroutine(couroutine);
+    }
+    private IEnumerator WaitVideoandLoadScene (float waitTime)
+    {
+    while (true)
+    {
+        yield return new WaitForSeconds ( waitTime );
+        print("WaitVideoandLoadScene" + Time.time );
+        SceneManager.LoadScene(1);
+
+    }
+}
+    private void Start()
+    {
+        mivideo.Play();
+        Cargando();
+    }
+}
