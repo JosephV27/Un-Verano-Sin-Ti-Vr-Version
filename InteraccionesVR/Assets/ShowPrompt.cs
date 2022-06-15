e15b4e72d2f7f43b7ac6a764ad06d99830d07fbc
Lucas <66132859+SilverSoul16@users.noreply.github.com>
2022-06-14 23:00:22 -0600

    Prompt traer objeto

---
+++ InteraccionesVR/Assets/ShowPrompt.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPrompt : MonoBehaviour
+{
+    public Canvas Prompt;
+    
+    void OnTriggerEnter(Collider ObjectEntering) {
+        if(ObjectEntering.tag == "Player") {
+            Prompt.enabled = true;
+        }
+    }
+    void onTriggerExit(Collider ObjectLeaving) {
+        if(ObjectLeaving.tag == "Player") {
+            Prompt.enabled = false;
+        }
+    }
+    }
+}
